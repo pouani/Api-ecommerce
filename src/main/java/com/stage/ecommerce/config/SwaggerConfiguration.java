f3c7db5871b926662482d168b37687cc42352896
ULRICH POUANI CHOUAMENI SIEMENOU <pouani.ulrich19@myiuc.com>
2023-05-11 19:35:28 +0200

    create configuration swagger

--- src/main/java/com/stage/ecommerce/config/SwaggerConfiguration.java
+++ src/main/java/com/stage/ecommerce/config/SwaggerConfiguration.java
@@ -1,4 +1,31 @@
 package com.stage.ecommerce.config;
 
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.stage.ecommerce.utils.IConstants.APP_ROOT;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfiguration {
+
+    public Docket apiConf(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(
+                        new ApiInfoBuilder()
+                                .description("API APPLICATION VENTE EN LIGNE documentation")
+                                .title("E-COMMERCE REST API")
+                                .build()
+                )
+                .groupName("REST API V1")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.stage.ecommerce"))
+                .paths(PathSelectors.ant(APP_ROOT+"/**"))
+                .build();
+    }
 }
