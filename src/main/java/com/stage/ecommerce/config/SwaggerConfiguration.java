5d64acba057b566a29b971be3e52dd986645dd63
ULRICH POUANI CHOUAMENI SIEMENOU <pouani.ulrich19@myiuc.com>
2023-05-12 17:40:07 +0200

    bug request

--- src/main/java/com/stage/ecommerce/config/SwaggerConfiguration.java
+++ src/main/java/com/stage/ecommerce/config/SwaggerConfiguration.java
@@ -6,30 +6,31 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static com.stage.ecommerce.utils.IConstants.APP_ROOT;
 
 @Configuration
 @EnableSwagger2
 @EnableWebMvc
 public class SwaggerConfiguration {
-
     @Bean
-    public Docket swaggerSpringfoxApiDocket(){
+    public Docket swaggerApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(
-                        new ApiInfoBuilder()
-                                .description("API APPLICATION VENTE EN LIGNE documentation")
-                                .title("E-COMMERCE REST API")
-                                .build()
-                )
-                .groupName("REST API V1")
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.stage.ecommerce"))
-                .paths(PathSelectors.ant(APP_ROOT+"/**"))
+                .apis(RequestHandlerSelectors.basePackage("com.stage.ecommerce")) //le package de base de vos contrôleurs
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("E-COMMERCE REST API") //le titre de votre API
+                .description("API APPLICATION VENTE EN LIGNE documentation") //la description de votre API
+                .version("1.0") //la version de votre API
                 .build();
     }
+
 }
